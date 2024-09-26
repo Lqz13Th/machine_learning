a8acfdafb94c21ea0307e0e039d6c0792972d47a
Lqz13Th <lqz13th@outlook.com>
2024-09-26 15:12:42 +0800

    finish price percent change bar

--- ml_class/price_percent_change_bar.py
+++ ml_class/price_percent_change_bar.py
@@ -2,12 +2,52 @@ import pandas as pd
 
 
 def px_pct_bar(
-    df: pd.DataFrame,
-):
-    last_price = df.iloc[1, 1]
-    print(last_price)
-    for i in range(len(df.iloc[:, 0].tolist())):
-        pass
+        df: pd.DataFrame,
+        threshold: float,
+) -> pd.DataFrame:
+    last_px = df.iloc[0]['price']
+    last_ts = df.iloc[0]['transact_time']
+
+    bars = []
+    ts_duration = 0
+    sum_buy_size = 0
+    sum_sell_size = 0
+
+    print(last_px)
+    for i in range(len(df)):
+        px = df.iloc[i]['price']
+        sz = df.iloc[i]['quantity']
+        ts = df.iloc[i]['transact_time']
+        side = -1 if df.iloc[i]['is_buyer_maker'] else 1  # 判断买卖方向 (True 为卖方主导，False 为买方主导)
+
+        px_pct = (px - last_px) / last_px
+
+        if side == 1:
+            sum_buy_size += sz
+
+        else:
+            sum_sell_size += sz
+
+        if abs(px_pct) > threshold:
+            ts_duration = ts - last_ts
+
+            bar = {
+                'price': px,
+                'sum_buy_size': sum_buy_size,
+                'sum_sell_size': sum_sell_size,
+                'timestamp_duration': ts_duration,
+                'price_pct_change': px_pct,
+                'change_side': 'Buy' if px_pct > 0 else 'Sell'
+            }
+            bars.append(bar)
+
+            last_px = px
+            last_ts = ts
+            sum_buy_size = 0
+            sum_sell_size = 0
+
+    bars_df = pd.DataFrame(bars)
+    return bars_df
 
 
 if __name__ == "__main__":
